4e19466f03d32e1463b13b491d74a340bf45dfc3
Kxffie <bryceriordan2005@gmail.com>
2025-05-07 09:52:07 -0500

    c

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://kxffiec.github.io',
+  base: '/ursinawebsite/',
   vite: { plugins: [tailwindcss()] },
   integrations: [
     react(), 
